a9bd13a489f1cbb8dbbc80197ad6eb45f1b6da19
githubuserx <githubuserx@users.noreply.github.com>
2018-12-27 21:41:37 +0100

    Remove unnecessary `file_exists()`

--- src/Shred.php
+++ src/Shred.php
@@ -114,14 +114,15 @@ final class Shred
    * @param string $filepath
    * @return bool
    */
-  private function fileWritable($filepath) {
-
+  private function fileWritable($filepath)
+  {
     // clear stat cache to avoid falsely reported file status
     // use $filename parameter to possibly improve performance
     clearstatcache(true, $filepath);
 
-    if (file_exists($filepath) && is_file($filepath)
-      && is_readable($filepath) && is_writable($filepath)) {
+    if (is_file($filepath)
+      && is_readable($filepath)
+      && is_writable($filepath)) {
       return true;
     }
 
